d8452c3d86cf68a76e16804173d80727cd0158be
mussa <math_200901@hotmail.com>
2024-08-28 21:32:45 -0300

    DataHora

---
+++ dataHora.py
@@ -0,0 +1,28 @@
+import requests
+import json
+import os
+
+def main() :
+    valConexao = 0
+
+    while valConexao != 200:
+        try:
+            resposta = requests.get('https://timeapi.io/api/time/current/zone?timeZone=America%2FSao_Paulo', timeout=5)
+            valConexao = resposta.status_code
+            if valConexao == 200:
+                dia = json.loads(resposta.text)["day"]
+                mes = json.loads(resposta.text)["month"]
+                ano = json.loads(resposta.text)["year"]
+                hora = json.loads(resposta.text)["hour"]
+                minuto = json.loads(resposta.text)["minute"]
+                segundo = json.loads(resposta.text)["seconds"]
+                os.system(f"date {dia}-{mes}-{ano}")
+                os.system(f"time {hora}:{minuto}:{segundo}")
+                
+            
+        except Exception as e:
+            valConexao = 0
+
+
+if __name__ == "__main__":
+    main()
